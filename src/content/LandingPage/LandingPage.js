7a6446d18b824242375a2a746390713cc6748963
ahmedmmeky <meky.wrk@gmail.com>
2022-03-30 18:55:23 -0400

    created welcome component for login or sign up.

--- src/content/LandingPage/LandingPage.js
+++ src/content/LandingPage/LandingPage.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Welcome from "../../components/Welcome";
+import styles from "./LandingPage.module.scss";
 
 const LandingPage = () => {
-  return <div>LandingPage</div>;
+  return (
+    <div className={styles.landingPage}>
+      <Welcome />
+    </div>
+  );
 };
 
 export default LandingPage;
