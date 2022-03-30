22c33d751f11c2cbe3c2ad996eeb5376996b2344
ahmedmmeky <meky.wrk@gmail.com>
2022-03-30 18:51:10 -0400

    added header to project.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,20 @@
 import React from "react";
+import styles from "./Header.module.scss";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className={styles.headerContainer}>
+      <div className={styles.headerLogo}>
+        <h2>Logo</h2>
+      </div>
+      <div className={styles.headerNav}>
+        <h3>Explore</h3>
+        <h3>Create</h3>
+        <h3>Profile</h3>
+        <h3>Wallet</h3>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
