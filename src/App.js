18898f6641f6b17fcb4034e56522ed28807555ed
ahmedmmeky <meky.wrk@gmail.com>
2022-03-31 16:11:09 -0400

    created explore component and routed to it.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./components/Header";
 import LandingPage from "./content/LandingPage";
+import Explore from "./content/Explore";
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <Router>
+        <Header />
         <Switch>
           <Route exact path="/" component={LandingPage}></Route>
+          <Route path="/explore" component={Explore}></Route>
         </Switch>
       </Router>
     </div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,18 @@
 import React from "react";
 import styles from "./Header.module.scss";
+import { Link } from "react-router-dom";
 
-const Header = () => {
+export const Header = () => {
   return (
     <div className={styles.headerContainer}>
       <div className={styles.headerLogo}>
         <h2>Logo</h2>
       </div>
       <div className={styles.headerNav}>
-        <h3>Explore</h3>
+        <Link to="/explore">
+          <h3>Explore</h3>
+        </Link>
+
         <h3>Create</h3>
         <h3>Profile</h3>
         <h3>Wallet</h3>
