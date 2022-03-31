41232f0d2990411780a3473a3947ee1c8866d2d7
ahmedmmeky <meky.wrk@gmail.com>
2022-03-31 15:38:11 -0400

    added login and sign up buttons.

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,7 +1,18 @@
 import React from "react";
-
+import Button from "@mui/material/Button";
+import styles from "./Welcome.module.scss";
 const Welcome = () => {
-  return <div>Welcome</div>;
+  return (
+    <div>
+      <div className={styles.logoAndButtons}>
+        <div className={styles.logoContainer}>Logo</div>
+        <div className={styles.buttonContainer}>
+          <Button variant="contained">Login</Button>
+          <Button variant="contained">Sign Up</Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Welcome;
