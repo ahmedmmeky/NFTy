da1fb681ca97bfb5de11fad757d854ca9cb1d824
ahmedmmeky <meky.wrk@gmail.com>
2022-04-14 16:53:51 -0400

    added empty modal for bid.

--- src/components/NFT/NFT.js
+++ src/components/NFT/NFT.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
@@ -7,11 +7,36 @@ import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import styles from "./NFT.module.scss";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
 
 const NFT = ({ name, price, description, imgUrl, expandedView = true }) => {
   const location = useLocation();
   const state = location.state;
   console.log(state);
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+    display: "flex",
+    justifyContent: "center",
+    gap: 32,
+  };
   return (
     <>
       {!expandedView && (
@@ -59,11 +84,26 @@ const NFT = ({ name, price, description, imgUrl, expandedView = true }) => {
               <h2>{state.description}</h2>
             </div>
             <div className={styles.subInfo}>
-              <Button variant="contained" size="small" color="primary">
+              <Button
+                variant="contained"
+                size="small"
+                color="primary"
+                onClick={() => setOpen(true)}
+              >
                 Place Bid
               </Button>
             </div>
           </div>
+
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+            className={styles.form}
+          >
+            <Box sx={style}></Box>
+          </Modal>
         </div>
       )}
     </>
